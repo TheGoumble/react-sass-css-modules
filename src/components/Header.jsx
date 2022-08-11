e5342a5459b831cdf40f646df7e77a6e1cfc2825
Javier Vargas <3231javiervargas@gmail.com>
2022-08-11 17:17:52 -0400

    using sass in react

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+export default function Header() {
+  return (
+    <>
+      <ul className="navigation">
+        <li>
+          <a href="">Home</a>
+        </li>
+        <li>
+          <a href="">About</a>
+        </li>
+        <li>
+          <a href="">Contact</a>
+        </li>
+        <li>
+          <a href="">Links</a>
+        </li>
+      </ul>
+
+      <div className="menu">
+        <a href="" className="menu__link">Link 1</a>
+        <a href="" className="menu__link">Link 2</a>
+        <a href="" className="menu__link">Link 3</a>
+      </div>
+    </>
+  )
+}
